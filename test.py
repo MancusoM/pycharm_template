90f06b3ad5bc19a728576d002e04862f17b5381b
MancusoM <73910562+MancusoM@users.noreply.github.com>
2024-12-01 21:00:13 -0500

    Elizabeth, if you're reading this commit message, you're truly insane at CS

--- test.py
+++ test.py
@@ -11,6 +11,15 @@ InsideJokes, Memories, Other = st.tabs(["Inside Jokes", "Memories", "Other"])
 with InsideJokes:
     st.write('test')
 
+    UnhingedThingsSFW,UnhingedThingsNSFW = st.tabs(['Unhinged Things Elizabeth has told me (SFW)','Unhinged Things Elizabeth has told me (NSFW)'])
+
+    with UnhingedThingsSFW:
+        st.write('test')
+
+        col1 =st.columns(1)
+
+    with UnhingedThingsNSFW:
+        st.write('test1')
 with Memories:
     st.write('test1')
 
