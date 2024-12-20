f8858e453e732d48248823e06dfc0857b983c4c6
MancusoM <73910562+MancusoM@users.noreply.github.com>
2024-12-19 19:00:53 -0500

    Adding Images to Test functionality

--- test.py
+++ test.py
@@ -1,27 +1,37 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
 st.title('Elizabeth Sarah McNaughton')
 
 container = st.container()
 
-(container.write('test'))
+(container.write('my description of her'))
 
-InsideJokes, Memories, Other = st.tabs(["Inside Jokes", "Memories", "Other"])
+Memories, Inside_Jokes = st.tabs(["Memories", "Inside Jokes"])
 
-with InsideJokes:
-    st.write('test')
-
-    UnhingedThingsSFW,UnhingedThingsNSFW = st.tabs(['Unhinged Things Elizabeth has told me (SFW)','Unhinged Things Elizabeth has told me (NSFW)'])
-
-    with UnhingedThingsSFW:
-        st.write('test')
-
-        col1 =st.columns(1)
-
-    with UnhingedThingsNSFW:
-        st.write('test1')
 with Memories:
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+
+        st.image('FCF4B0DE-756C-4597-922C-80C18319C316_1_105_c.jpeg',caption ="Lizzy's First Appearance on Camera Roll", width= 200)
+        st.image('D677062A-24CD-42AE-80B2-680571D608CE_1_105_c.jpeg', caption="Our First Met Date",
+                 width=200)
+        st.image('30D16FB5-0810-49BC-AD86-A0816580D0A0_1_105_c.jpeg', caption="Lizzy's First Appearance on Camera Roll",
+                 width=200)
+    with col2:
+        st.image('FCF4B0DE-756C-4597-922C-80C18319C316_1_105_c.jpeg', caption="Lizzy's First Appearance on Camera Roll",
+             width=200)
+
+    with col3:
+        st.image('FCF4B0DE-756C-4597-922C-80C18319C316_1_105_c.jpeg', caption="Lizzy's First Appearance on Camera Roll",
+                 width=200)
+        st.image('FCF4B0DE-756C-4597-922C-80C18319C316_1_105_c.jpeg', caption="Lizzy's First Appearance on Camera Roll",
+             width=200)
+
+
+
+with Inside_Jokes:
     st.write('test1')
 
-with Other:
-    st.write('test4')
